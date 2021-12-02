0cb579af348673b63ec659310a78fe90f84084df
Guilherme <guilherme.gg1@hotmail.com>
2021-12-02 13:33:52 -0300

    ci: pre install dependencies

---
+++ pre-install.sh
@@ -0,0 +1,12 @@
+# dependencies
+sudo pacman -Sy fzf stow python3
+
+# nodeenv
+yay -Sy nvm
+source /usr/share/nvm/init-nvm.sh
+nvm install --lts
+npm install -g yarn neovim
+
+# neovim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
