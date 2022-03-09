68eadf093429c8590b8c9bbbe4b19ede1f97ee2b
guigallo <guilherme.gg1@hotmail.com>
2022-03-09 11:56:03 -0300

    wsl: config permissions on startup wsl

---
+++ wsl/wslboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# WSL startup script
+
+# Set correct ownership and permissions for /dev/kvm
+# this script should be added at '/etc/wsl.conf':
+# [boot]
+# command=/this/file/path
+chown root:kvm /dev/kvm
+chmod 0660 /dev/kvm
+echo '[kvm] permission set'
+
+# Set correct permissions for docker
+chmod 666 /var/run/docker.sock
+echo '[docker] permission set'
